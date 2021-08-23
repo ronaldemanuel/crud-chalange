40fc1216c2409fb023ff9cd90464f97d72ffa2d2
ronaldemanuel <emronald15el@gmail.com>
2021-08-23 00:03:20 -0300

    feat: create user seeder

---
+++ database/seeders/User.ts
@@ -0,0 +1,19 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class UserSeeder extends BaseSeeder {
+  public async run() {
+    await User.createMany([
+      {
+        userName: 'jorjinDaMassa',
+        email: 'jorjin@email.com',
+        password: '12345',
+      },
+      {
+        userName: 'clyeton',
+        email: 'cleyton@email.com',
+        password: '12345',
+      },
+    ])
+  }
+}
