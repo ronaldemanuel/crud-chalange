e11c6db24bac559946124b1b25ec086bb2bc1ac5
ronaldemanuel <emronald15el@gmail.com>
2021-08-23 14:48:25 -0300

    feat: implement index function in users controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,6 +3,12 @@ import User from 'App/Models/User'
 import StoreValidator from 'App/Validators/User/StoreValidator'
 
 export default class UsersController {
+  // eslint-disable-next-line prettier/prettier
+  public async index({ }) {
+    const users = await User.query()
+    return users
+  }
+
   public async store({ request }: HttpContextContract) {
     const data = await request.validate(StoreValidator)
     const user = await User.create(data)
