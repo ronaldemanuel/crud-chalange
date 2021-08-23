9c26c38cf6626df9762add3a108a78b02ecf7213
ronaldemanuel <emronald15el@gmail.com>
2021-08-23 09:40:01 -0300

    chore: add encryption to passwords

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,3 +1,4 @@
+import Encryption from '@ioc:Adonis/Core/Encryption'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import User from 'App/Models/User'
 
@@ -7,17 +8,17 @@ export default class UserSeeder extends BaseSeeder {
       {
         userName: 'jorjinDaMassa',
         email: 'jorjin@email.com',
-        password: '12345',
+        password: Encryption.encrypt('12345678'),
       },
       {
         userName: 'clyeton',
         email: 'cleyton@email.com',
-        password: '12345',
+        password: Encryption.encrypt('12345678'),
       },
       {
         userName: 'adamastor',
         email: 'adamastor@email.com',
-        password: '12345',
+        password: Encryption.encrypt('12345678'),
       },
     ])
   }
