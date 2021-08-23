bd52a714b7b3dedb249951f98a5b4590b853cfc5
ronaldemanuel <emronald15el@gmail.com>
2021-08-23 14:56:56 -0300

    feat: implement user show route

--- start/routes.ts
+++ start/routes.ts
@@ -22,3 +22,4 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/users', 'UsersController.index')
 Route.post('/users', 'UsersController.store')
+Route.get('/users/:id', 'UsersController.show')
