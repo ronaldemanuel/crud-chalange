d74b375d049cb0bffec2621030ef3478f2673fe3
ronaldemanuel <emronald15el@gmail.com>
2021-08-28 10:10:51 -0300

    feat: add slug column on role migration

--- database/migrations/1630114185334_roles.ts
+++ database/migrations/1630114185334_roles.ts
@@ -6,6 +6,7 @@ export default class Roles extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('slug').notNullable().unique()
       table.string('name').notNullable().unique()
       table.text('description').notNullable()
       table.timestamp('created_at', { useTz: true })
