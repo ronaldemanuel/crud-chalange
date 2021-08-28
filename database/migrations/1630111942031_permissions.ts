b40bb888dee6a5fed5a471b791adfeeac9a9e680
ronaldemanuel <emronald15el@gmail.com>
2021-08-27 22:55:00 -0300

    fix: fix the creation of the name and description fields in the permission migration

--- database/migrations/1630111942031_permissions.ts
+++ database/migrations/1630111942031_permissions.ts
@@ -6,8 +6,8 @@ export default class Permissions extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name')
-      table.text('description')
+      table.string('name').notNullable().unique()
+      table.text('description').notNullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
