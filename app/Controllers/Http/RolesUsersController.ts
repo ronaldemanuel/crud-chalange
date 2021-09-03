8984bf4f44337018a28f6b940850f63b7f85c187
ronaldemanuel <emronald15el@gmail.com>
2021-09-03 11:23:18 -0300

    feat: implement store function for roles users controller

--- app/Controllers/Http/RolesUsersController.ts
+++ app/Controllers/Http/RolesUsersController.ts
@@ -1,5 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Role from 'App/Models/Role'
 import User from 'App/Models/User'
+import StoreValidator from 'App/Validators/RolesUsers/StoreValidator'
+import UpdateValidator from 'App/Validators/RolesUsers/UpdateValidator'
 
 export default class RoleUsersController {
   public async index({ params, response }: HttpContextContract) {
@@ -10,6 +13,18 @@ export default class RoleUsersController {
       return roles
     } catch (error) {
       return response.status(404).send({ error: 'relation not found' })
+    }
+  }
+
+  public async store({ params, response, request }: HttpContextContract) {
+    const data = await request.validate(StoreValidator)
+    try {
+      const user = await User.findOrFail(params.user_id)
+      const role = await Role.findOrFail(data.id)
+
+      user.related('roles').attach([role.id])
+    } catch (error) {
+      return response.status(404).send({ error: 'object not found' })
     }
   }
 }
