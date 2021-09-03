47d806df2ea75e577bc7cd26938ac09c0e9eb964
ronaldemanuel <emronald15el@gmail.com>
2021-09-03 11:05:36 -0300

    feat: implement store validator for roles user controller

---
+++ app/Validators/RolesUsers/StoreValidator.ts
@@ -0,0 +1,16 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class StoreValidator {
+  constructor(protected ctx: HttpContextContract) {
+    return
+  }
+
+  public schema = schema.create({
+    id: schema.number([rules.required()]),
+  })
+
+  public messages = {
+    'id.required': 'É obrigatório passar o id da regra',
+  }
+}
