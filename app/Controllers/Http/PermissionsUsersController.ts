9144ff24ba31dc4a5daea5cb6739dcaab4a30868
ronaldemanuel <emronald15el@gmail.com>
2021-09-09 20:08:26 -0300

    feat: implement store function for permissions users controller

--- app/Controllers/Http/PermissionsUsersController.ts
+++ app/Controllers/Http/PermissionsUsersController.ts
@@ -1,5 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Permission from 'App/Models/Permission'
 import User from 'App/Models/User'
+import StoreValidator from 'App/Validators/PermissionsUsers/StoreValidator'
+
 export default class PermissionsUsersController {
   public async index({ params, response }: HttpContextContract) {
     try {
@@ -12,4 +15,15 @@ export default class PermissionsUsersController {
     }
   }
 
+  public async store({ params, request, response }: HttpContextContract) {
+    const data = await request.validate(StoreValidator)
+    try {
+      const user = await User.findOrFail(params.user_id)
+      const permission = await Permission.findOrFail(data.id)
+
+      user.related('permissions').attach([permission.id])
+    } catch (error) {
+      return response.status(404).send({ error: 'object not found' })
+    }
+  }
 }
